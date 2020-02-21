3f0769b00d2467c6ffd84e52414618c5f2bb139c
fizzday <fizzday@yeah.net>
2020-02-21 10:13:08 +0000

    new'

--- helper/helper.go
+++ helper/helper.go
@@ -1,12 +1,39 @@
 package helper
 
-func StartWith(src,short string) bool {
-	if len(src)< len(short) {
-		return false
-	}
+import "github.com/gohouse/t"
 
-	if src[:len(short)]==short{
-		return true
+// Max 获取最大的数
+func Max(args ...interface{}) int {
+	if len(args) == 0 {
+		return 0
 	}
-	return false
+	var max = t.New(args[0]).Int()
+	for k, arg := range args {
+		if k == 0 {
+			continue
+		}
+		tmp := t.New(arg).Int()
+		if tmp > max {
+			max = tmp
+		}
+	}
+	return max
+}
+
+// Min 获取最小的数
+func Min(args ...interface{}) int {
+	if len(args) == 0 {
+		return 0
+	}
+	var min = t.New(args[0]).Int()
+	for k, arg := range args {
+		if k == 0 {
+			continue
+		}
+		tmp := t.New(arg).Int()
+		if tmp < min {
+			min = tmp
+		}
+	}
+	return min
 }
